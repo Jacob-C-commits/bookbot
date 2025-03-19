f270bc647210305e4c49a60225d3c019b4074bab
Jacob Christian <jachristian31@gmail.com>
2025-03-19 14:55:39 -0400

    added character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 def get_books_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,6 +9,9 @@ def main(book):
     text = get_books_text(book)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+
+    char_count = get_char_count(text)
+    print(char_count)
 
 main("books/frankenstein.txt")
 
--- stats.py
+++ stats.py
@@ -3,3 +3,11 @@ def get_num_words(text):
     word_list = text.split()
     num_words = len(word_list)
     return num_words
+
+def get_char_count(text):
+    char_count = {}
+    low_text = text.lower()
+    for char in low_text:
+        char_count[char] = char_count.get(char, 0) + 1
+    
+    return char_count
